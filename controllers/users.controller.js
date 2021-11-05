0c36e053075180736db6c2d53757766142eb0004
JefersonC <Jeffer11camacho@gmal.com>
2021-11-05 11:26:56 -0500

    Implement jwt redis

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,9 @@
 const User = require("../models/Users.model");
 const errorResponse = require("../helper/errorResponse");
+var redis = require('');
+var JWTR =  require('jwt-redis').default;
+var redisClient = redis.createClient();
+var jwtr = new JWTR(redisClient);
 
 exports.createUser = async (req, res, next) => {
   try {
